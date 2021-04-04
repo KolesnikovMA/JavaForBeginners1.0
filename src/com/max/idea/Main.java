ab0666e8eb6d9052907f8d2fce1c07c3fff0e371
KolesnikovMA <retbert@yandex.com>
2021-04-04 23:32:51 +0300

    task#3

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -5,5 +5,11 @@ public class Main {
         System.out.println("Hello World!");
         double a = 5.4;
         byte b = 127;
+
+        int[] nums = {1, 2, 3, 4, 5};
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        System.out.println(nums[0] + nums[nums.length / 2]);
     }
 }
